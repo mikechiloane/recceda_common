f266a6c3a4cd5bd2a6027c2d86829dd473c0ebef
Mike Chiloane <125856129+chiloanemike@users.noreply.github.com>
2025-03-30 00:23:20 +0200

    Add accessors to form response fields

--- src/main/java/com/recceda/common/response/form/FormResponse.java
+++ src/main/java/com/recceda/common/response/form/FormResponse.java
@@ -1,9 +1,11 @@
 package com.recceda.common.response.form;
 
 import com.recceda.common.form.FormField;
+import lombok.Data;
 
 import java.util.List;
 
+@Data
 public class FormResponse {
     private String formId;
     private String formName;
